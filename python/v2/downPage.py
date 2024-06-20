aeda9a6ade4d9da08e3d4564f3d5089e9e6c91ed
André Costa <andresilvacosta1992@gmail.com>
2024-06-20 17:12:24 -0300

    commit automático

--- python/v2/downPage.py
+++ python/v2/downPage.py
@@ -11,9 +11,10 @@ def setup_driver():
     chrome_options = Options()
     #proxy_ip_port = '66.78.34.223:5842'
     #chrome_options.add_argument(f'--proxy-server={proxy_ip_port}')
-    #chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--window-size=1920x1080") 
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     #chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
