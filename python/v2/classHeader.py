72d28dffaad19211e74b429227ca882f3b6bca95
André Costa <andresilvacosta1992@gmail.com>
2024-06-20 18:53:32 -0300

    commit automático

--- python/v2/classHeader.py
+++
@@ -1,5 +0,0 @@
-from Class import extrairClass
-
-def classHeader():
-    extrairClass('primario', 'ssp-header-area', 'classHeader')
-    
--- python/v2/extrairUnidades.py
+++ python/v2/extrairUnidades.py
@@ -1,1 +1,2 @@
 #criar uma lista de cursos existentes do senac e salvar em data/listaCursos.json
+#importar o data/primario/paginaPrincipal
