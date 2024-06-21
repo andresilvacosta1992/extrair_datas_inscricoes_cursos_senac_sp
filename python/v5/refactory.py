7920f17d28d61bc29f235e0e1cb314d312609620
André Costa <andresilvacosta1992@gmail.com>
2024-06-21 16:35:14 -0300

    commit automático

--- python/v5/refactory.py
+++ python/v5/refactory.py
@@ -1,25 +1,21 @@
 import os
 import time
-import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from pyvirtualdisplay import Display
+from bs4 import BeautifulSoup
+import json
+import unidecode
+import shutil 
 
-
-def setup_driver():
-    
+def setup_driver():    
     chrome_options = Options()
-    #proxy_ip_port = '66.78.34.223:5842'
-    #chrome_options.add_argument(f'--proxy-server={proxy_ip_port}')
-    #chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--window-size=1920x1080") 
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-    #chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36")
     chrome_options.add_argument("accept-language=en-US,en;q=0.9")
     chrome_options.add_argument("accept=text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
@@ -54,9 +50,6 @@ def downPagePrincipal():
     print(f"fazendo download da página principal '{link}'")
     downloadPagina(link, arquivo, pasta)
 
-import sys
-from bs4 import BeautifulSoup
-import os
 
 def extrairClass(arquivoEntrada, tagClass, arquivoSaida):
     os.makedirs(os.path.dirname(arquivoSaida), exist_ok=True)
@@ -77,9 +70,7 @@ def extrairClass(arquivoEntrada, tagClass, arquivoSaida):
 def menu():
     extrairClass('data/paginaPrincipal.html', 'ssp-mega-menu__wrapper', 'data/menu.html')
 
-from bs4 import BeautifulSoup
-import os
-import json 
+
 
 def areas(file_path='data/menu.html', output_path='data/areas.json'):
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
@@ -110,9 +101,55 @@ def download_areas():
         print(f"foi extraído o html da class 'nav-btn-filter' do arquivo f'data/subAreas/subAreas1/{area}.html' salvo no f'data/subAreas/subAreas2/{area}.html")
 
 
+def slugify(text):
+    # Transforma texto em slug: minúsculas, sem acentos, espaços por hifens
+    text = unidecode.unidecode(text).lower()  # Remover acentos e converter para minúsculas
+    text = text.replace(' ', '-')  # Substituir espaços por hifens
+    return text
+
+def subAreas3():
+    # Carregar as áreas do arquivo JSON
+    with open('data/areas.json', 'r', encoding='utf-8') as file:
+        areas = json.load(file)
+
+    # Dicionário para armazenar as subáreas por área
+    area_subareas = {}
+
+    for area in areas:
+        input_path = f"data/subAreas/subAreas2/{area}.html"
+        os.makedirs('data/subAreas/subAreas3', exist_ok=True)
+
+        try:
+            with open(input_path, 'r', encoding='utf-8') as file:
+                conteudo = file.read()
+
+            # Parsear o conteúdo HTML com BeautifulSoup
+            soup = BeautifulSoup(conteudo, 'html.parser')
+
+            # Encontrar todos os elementos <a> com o atributo data-tema
+            subareas = soup.find('nav', id='nav-sub-temas').find_all('a', attrs={"data-tema": True})
+
+            # Adicionar subáreas ao dicionário usando a área como chave
+            area_subareas[area] = [slugify(subarea.text) for subarea in subareas]
+        
+        except FileNotFoundError:
+            print(f"Erro: O arquivo {input_path} não foi encontrado.")
+        except Exception as e:
+            print(f"Erro ao processar o arquivo {input_path}: {e}")
+
+    # Salvar o dicionário de subáreas em um arquivo JSON
+    output_path = 'data/subAreas/subAreas3/subareas.json'
+    with open(output_path, 'w', encoding='utf-8') as file:
+        json.dump(area_subareas, file, ensure_ascii=False, indent=4)
+    print(f"Subáreas extraídas e salvas em JSON em: {output_path}")
+
+    # Copiar o arquivo JSON para a pasta data/
+    destination_path = 'data/subareas.json'
+    shutil.copy(output_path, destination_path)
+    print(f"Cópia do arquivo JSON salva em: {destination_path}")
 
 if __name__ == "__main__":
     #downPagePrincipal()
     #menu()
     #areas()
-    #download_areas()
+    download_areas()
