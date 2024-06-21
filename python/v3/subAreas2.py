5fa693413492e57ecf2aa31e020f534b7b28cabd
André Costa <andresilvacosta1992@gmail.com>
2024-06-20 22:00:43 -0300

    commit automático

--- python/v3/subAreas2.py
+++ python/v3/subAreas2.py
@@ -5,20 +5,16 @@ from downPage import downloadPagina
 def subAreas2():
     # Importar o array de áreas em data/areas.json
     with open('data/areas.json', 'r', encoding='utf-8') as file:
-        areas = json.load(file)
-    
+        areas = json.load(file)    
     # Diretório para salvar os downloads
-    save_directory = 'data/cache/areas'
-    
+    save_directory = 'cache/areas'    
     # Certifique-se de que o diretório para salvar os downloads existe
-    os.makedirs(save_directory, exist_ok=True)
-    
+    os.makedirs(save_directory, exist_ok=True)    
     # Para cada slug de área, fazer o download da página
     for area in areas:
         url = f"https://www.sp.senac.br/areas/{area}"
         file_name = area
         downloadPagina(url, file_name, save_directory)
-
 # Execute a função se este script for o principal executado
 if __name__ == "__main__":
     subAreas2()
