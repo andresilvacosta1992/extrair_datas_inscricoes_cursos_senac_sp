85caa5aca95eaac54b10e59d7a03d24c74105f2f
André Costa <andresilvacosta1992@gmail.com>
2024-06-21 01:02:54 -0300

    commit automático

--- python/v4/linksSubAreas1.py
+++ python/v4/linksSubAreas1.py
@@ -11,14 +11,14 @@ def linksSubAreas1():
 
     # Gerar links para cada subárea
     for area, subareas_list in subareas.items():
-        links = [f"https://www.sp.senac.br/cursos-livres/{area}/{subarea}" for subarea in subareas_list]
+        links = [f"https://www.sp.senac.br/cursos-livres/{area}" for subarea in subareas_list]
         links_subareas[area] = links
 
     # Garantir que o diretório de destino existe
     os.makedirs('data/linksSubAreas', exist_ok=True)
 
     # Salvar os links em um arquivo JSON
-    with open('data/linksSubAreas/linksSubAreas1.json', 'w', encoding='utf-8') as file:
+    with open('data/linksSubAreas/linksSubAreas3.json', 'w', encoding='utf-8') as file:
         json.dump(links_subareas, file, ensure_ascii=False, indent=4)
 
     print("Links das subáreas foram salvos em 'data/linksSubAreas.json'")
--- python/v4/linksSubAreas2.py
+++ python/v4/linksSubAreas2.py
@@ -12,7 +12,7 @@ def linksSubAreas2():
 
     # Gerar links para cada subárea
     for area, subareas_list in subareas.items():
-        links = [f"https://www.sp.senac.br/cursos-tecnicos/{area}/{subarea}" for subarea in subareas_list]
+        links = [f"https://www.sp.senac.br/cursos-tecnicos/curso-tecnico-em-{subarea}" for subarea in subareas_list]
         links_subareas[area] = links
 
     # Garantir que o diretório de destino existe
@@ -22,7 +22,7 @@ def linksSubAreas2():
     with open('data/linksSubAreas/linksSubAreas2.json', 'w', encoding='utf-8') as file:
         json.dump(links_subareas, file, ensure_ascii=False, indent=4)
 
-    print("Links das subáreas foram salvos em 'data/linksSubAreas.json'")
+    print("Links das subáreas foram salvos em 'data/linksSubAreas/linksSubAreas2.json'")
 
 if __name__ == "__main__":
     linksSubAreas2()
