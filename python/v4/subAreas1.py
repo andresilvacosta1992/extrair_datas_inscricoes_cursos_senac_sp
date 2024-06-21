21c72df7a83a602123e4556796b8091d0a12b452
André Costa <andresilvacosta1992@gmail.com>
2024-06-20 23:52:05 -0300

    commit automático

--- python/v4/subAreas1.py
+++ python/v4/subAreas1.py
@@ -1,7 +1,4 @@
-#download de todos as páginas de áreas
-
 import json
-import os
 from downPage import downloadPagina
 
 def download_areas():
@@ -9,16 +6,11 @@ def download_areas():
     with open('data/areas.json', 'r', encoding='utf-8') as file:
         areas = json.load(file)
     
-    # Diretório para salvar os downloads das páginas
-    save_directory = 'subAreas/subAreas1'
-
-    
-    # Fazer download de cada página de área e salvar o conteúdo
     for area in areas:
         url = f"https://www.sp.senac.br/areas/{area}"
         file_name = area
-        downloadPagina(url, file_name, save_directory)
-        print(f"Download da página '{area}' concluído e salvo em '{save_directory}/{file_name}'")
+        downloadPagina(url, file_name, 'subAreas/subAreas1')
+        print(f"Download da página '{area}' concluído e salvo em 'subAreas/subAreas1'")
 
 if __name__ == "__main__":
     download_areas()
