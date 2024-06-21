60c78ae15faf3ba4676eace1d6d05a192cdbae42
André Costa <andresilvacosta1992@gmail.com>
2024-06-21 00:57:51 -0300

    commit automático

--- python/v4/linksSubAreas2.py
+++ python/v4/linksSubAreas2.py
@@ -1,7 +1,8 @@
+#cursos técnicos
 import json
 import os
 
-def linksSubAreas1():
+def linksSubAreas2():
     # Carregar as subáreas do arquivo JSON
     with open('data/subareas.json', 'r', encoding='utf-8') as file:
         subareas = json.load(file)
@@ -11,17 +12,17 @@ def linksSubAreas1():
 
     # Gerar links para cada subárea
     for area, subareas_list in subareas.items():
-        links = [f"https://www.sp.senac.br/cursos-livres/{area}/{subarea}" for subarea in subareas_list]
+        links = [f"https://www.sp.senac.br/cursos-tecnicos/{area}/{subarea}" for subarea in subareas_list]
         links_subareas[area] = links
 
     # Garantir que o diretório de destino existe
     os.makedirs('data/linksSubAreas', exist_ok=True)
 
     # Salvar os links em um arquivo JSON
-    with open('data/linksSubAreas/linksSubAreas1.json', 'w', encoding='utf-8') as file:
+    with open('data/linksSubAreas/linksSubAreas2.json', 'w', encoding='utf-8') as file:
         json.dump(links_subareas, file, ensure_ascii=False, indent=4)
 
     print("Links das subáreas foram salvos em 'data/linksSubAreas.json'")
 
 if __name__ == "__main__":
-    linksSubAreas1()
+    linksSubAreas2()
